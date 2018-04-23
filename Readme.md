ac9aa8ad9cb3f53a9a804ade1995bc33a5934901
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to change the current NavigationPane behavior to make it look like "hamburger menu" from mobile applications
+
+
+<p>The "hamburger menu" is a popular menu control for modern mobile applications. We have <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraBarsNavigationNavigationPanetopic">NavigationPane</a> whose layout looks similar to this "hamburger menu", but has several differences. So, this example demonstrates how to customize the current NavigationPane behavior to make it look like the mentioned menu for modern mobile applications.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-change-the-current-navigationpane-behavior-to-make-it-look-like-hamburger-menu-from-t321985/15.2.9+/media/3bba46dd-9da6-11e5-80bf-00155d62480c.png"></p>
+
+<br/>
+
+
---
+++ VB/WindowsFormsApplication7/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Imports DevExpress.XtraBars.Docking
+Imports DevExpress.XtraBars.Docking2010
+Imports DevExpress.XtraBars.Navigation
+Imports DevExpress.XtraEditors
+
+Namespace HamburgerMenu
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            navigationPane1.PageProperties.ShowMode = ItemShowMode.Image
+            AddHandler navigationPane1.StateChanged, AddressOf navigationPane1_StateChanged
+            Dim button As New CustomNavigationButton(navigationPane1)
+            Dim buttonPanel As ButtonsPanel = TryCast((TryCast(navigationPane1, INavigationPane)).ButtonsPanel, ButtonsPanel)
+            buttonPanel.Buttons.Insert(0, button)
+
+            For i As Integer = 0 To buttonPanel.Buttons.Count - 1
+                CType(buttonPanel.Buttons(i), NavigationButton).VisibleIndex = i
+            Next i
+
+            AddHandler buttonPanel.ButtonClick, AddressOf ButtonsPanel_ButtonClick
+        End Sub
+
+        Private Sub navigationPane1_StateChanged(ByVal sender As Object, ByVal e As StateChangedEventArgs)
+            Dim navPane As NavigationPane = TryCast(sender, NavigationPane)
+
+            If e.State = NavigationPaneState.Collapsed Then
+                navPane.PageProperties.ShowMode = ItemShowMode.Image
+                navPane.LayoutChanged()
+                Dim newSize As Size = (TryCast(navPane, INavigationPane)).RegularMinSize
+                navPane.Width = newSize.Width - NavigationPane.StickyWidth
+            End If
+        End Sub
+        Private Sub ButtonsPanel_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.Docking2010.BaseButtonEventArgs)
+            Dim panel As NavigationPaneButtonsPanel = TryCast(sender, NavigationPaneButtonsPanel)
+            Dim navPane As NavigationPane = TryCast(panel.Owner, NavigationPane)
+
+            If navPane.State = NavigationPaneState.Collapsed Then
+                navPane.State = NavigationPaneState.Default
+                Return
+            End If
+            If navPane.PageProperties.ShowMode = ItemShowMode.Image Then
+                navPane.PageProperties.ShowMode = ItemShowMode.ImageAndText
+            Else
+                navPane.PageProperties.ShowMode = ItemShowMode.Image
+            End If
+        End Sub
+    End Class
+    Public Class CustomNavigationButton
+        Inherits NavigationButton
+
+        Private paneCore As NavigationPane
+        Public Sub New(ByVal navigationPane As NavigationPane)
+            MyBase.New(navigationPane.SelectedPage)
+            paneCore = navigationPane
+        End Sub
+        Public Overrides ReadOnly Property UseCaption() As Boolean
+            Get
+                Return paneCore.PageProperties.ShowMode = ItemShowMode.ImageAndText OrElse paneCore.PageProperties.ShowMode = ItemShowMode.Text OrElse ((paneCore.PageProperties.ShowMode = ItemShowMode.ImageOrText OrElse paneCore.PageProperties.ShowMode = ItemShowMode.Default) And Image Is Nothing)
+            End Get
+        End Property
+        Public Overrides ReadOnly Property UseImage() As Boolean
+            Get
+                Return paneCore.PageProperties.ShowMode <> ItemShowMode.Text
+            End Get
+        End Property
+        Public Overrides ReadOnly Property Style() As ButtonStyle
+            Get
+                Return ButtonStyle.PushButton
+            End Get
+        End Property
+        Public Overrides ReadOnly Property Image() As Image
+            Get
+                Return My.Resources.menu1
+            End Get
+        End Property
+        Public Overrides ReadOnly Property Caption() As String
+            Get
+                Return "Menu"
+            End Get
+        End Property
+        Public Overrides ReadOnly Property Visible() As Boolean
+            Get
+                Return True
+            End Get
+        End Property
+    End Class
+End Namespace
